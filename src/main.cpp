d2692570bcee174e99e00d905110aa36d289d34e
Michael Reuter <reutertu3@gmail.com>
2024-09-02 02:32:51 +0200

    Changes for the lab

--- src/main.cpp
+++ src/main.cpp
@@ -2416,6 +2416,7 @@ void setup()
   esp_partition_iterator_t   pi ;                         // Iterator for find
   const esp_partition_t*     ps ;                         // Pointer to partition struct
 
+  //nvs_flash_init();
   maintask = xTaskGetCurrentTaskHandle() ;                // My taskhandle
   outchunk.datatyp = QDATA ;                              // This chunk dedicated to QDATA
   vTaskDelay ( 3000 / portTICK_PERIOD_MS ) ;              // Wait for PlatformIO monitor to start
@@ -2668,7 +2669,7 @@ void setup()
     gettime() ;                                           // Sync time
   }
   adc1_config_width ( ADC_WIDTH_12Bit ) ;
-  adc1_config_channel_atten ( ADC1_CHANNEL_0, ADC_ATTEN_DB_11 ) ;  // VP/GPIO36
+  adc1_config_channel_atten ( ADC1_CHANNEL_0, ADC_ATTEN_DB_12 ) ;  // VP/GPIO36
   xTaskCreatePinnedToCore (
     playtask,                                             // Task to play data in dataqueue.
     "Playtask",                                           // Name of task.
